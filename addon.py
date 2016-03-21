d6a190e04f219165ae37000169f2a4ed4cadc055
William Forde <willforde+github@gmail.com>
2016-03-21 22:06:00 +0000

    Massive overhaul

--- addon.py
+++ addon.py
@@ -9,21 +9,21 @@
 	
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
 	
 	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-def execute():
-	import xbmcutil
-	action = xbmcutil.plugin.actions[0]
-	if action == u"system": xbmcutil.sysCheck()
-	else:
-		# Call Function Based on Action Param
-		import main as plugin
-		getattr(plugin, action)()
+# Standard Library Imports
+import codequick
+
+@codequick.route("/")
+class Root(codequick.YoutubeBase):
+	def channelID(self):
+		return "UCnavGPxEijftXneFxk28srA"
 
 # Initiate Startup
-if __name__ == "__main__": execute()
+if __name__ == "__main__":
+	codequick.run(debug=True)
