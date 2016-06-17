5c5aaa4e2dc6db86f0bda41bb525b3528d96da28
William Forde <willforde@gmail.com>
2016-06-17 19:05:06 +0100

    Code reformat

--- addon.py
+++ addon.py
@@ -1,29 +1,30 @@
 """
-	Copyright: (c) 2013 William Forde (willforde+xbmc@gmail.com)
-	License: GPLv3, see LICENSE for more details
-	
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program. If not, see <http://www.gnu.org/licenses/>.
+Copyright: (c) 2013 William Forde (willforde+xbmc@gmail.com)
+License: GPLv3, see LICENSE for more details
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 # Standard Library Imports
 import codequick
 
+
 @codequick.route("/")
 class Root(codequick.YoutubeBase):
-	def channelID(self):
-		return "UCnavGPxEijftXneFxk28srA"
+    def channelID(self):
+        return "UCnavGPxEijftXneFxk28srA"
 
 # Initiate Startup
 if __name__ == "__main__":
-	codequick.run(debug=True)
+    codequick.run(debug=True)
