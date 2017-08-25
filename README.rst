52fb522e3a0ac8debaf1ae1ede70ed2d0be17db1
William Forde <willforde@gmail.com>
2017-08-25 01:24:52 +0100

    full upgrade

---
+++ README.rst
@@ -0,0 +1,9 @@
+
+plugin.video.earthtouch
+-----------------------
+This is a kodi video add-on for Earth Touch nature videos. https://www.earthtouchnews.com
+See nature's drama unfold on Earth Touch.
+
+Earth Touch News Network is a platform for the latest news and emerging stories about the natural world. Writers,
+filmmakers and other contributors scattered across the globe are brought together by a common mission to tell those
+stories simply and accurately, so they can reach like-minded nature lovers anywhere.
--- addon.py
+++ addon.py
@@ -1,28 +1,30 @@
-"""
-Copyright: (c) 2016 William Forde (willforde+kodi@gmail.com)
-License: GPLv3, see LICENSE for more details
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2016 William Forde (willforde+kodi@gmail.com)
+# License: GPLv3, see LICENSE for more details
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# Package imports
+from codequick import youtube, Route, run
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
+@Route.register
+class Root(youtube.Playlists):
+    def run(self, **kwargs):
+        return super(Root, self).run("UCnavGPxEijftXneFxk28srA", loop=True, **kwargs)
 
-# Package Import
-from codequick import youtube, route, run
 
-@route("/")
-def root():
-    return youtube.playlists("UCnavGPxEijftXneFxk28srA")
-
-# Initiate Startup
+# Initiate add-on
 if __name__ == "__main__":
     run()
